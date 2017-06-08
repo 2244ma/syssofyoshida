4cc51c27a5faa826b0355e240122128f8ddadbcb
yoshi <hogehoge@aiit.ac.jp>
2017-06-08 20:24:05 +0900

    Push kadai3
    kadai3 & test push deleted exfile

--- 17745145.sh
+++ 17745145.sh
@@ -10,21 +10,21 @@ ERROR_EXIT () {
 
 # check input 2 numbers
 echo kazu,tarinai > $tmp-ans
-./17745145.sh 100 > $tmp-err && ERROR_EXIT "we can't script"
+./kadai2.sh 100 > $tmp-err && ERROR_EXIT "we can't script"
 diff $tmp-ans $tmp-err || ERROR_EXIT "err_msg diff"
 
 echo test1 OK
 
 # check 0 number
 echo "Don't input 0" > $tmp-ans
-./17745145.sh 0 10 > $tmp-err && ERROR_EXIT "we can't script"
+./kadai2.sh 0 10 > $tmp-err && ERROR_EXIT "we can't script"
 diff $tmp-ans $tmp-err || ERROR_EXIT "17745145 0 correct?"
 
 echo test2 OK
 
 # hikisu kazu test
 echo requiared 2 numbers > $tmp-ans
-./17745145.sh 100 200 3 > $tmp-err && ERROR_EXIT "we can't script"
+./kadai2.sh 100 200 3 > $tmp-err && ERROR_EXIT "we can't script"
 diff $tmp-ans $tmp-err || ERROR_EXIT "17745145 hikisu correct?"
 
 echo "test3 OK"
@@ -32,7 +32,7 @@ echo "test3 OK"
 # moji check
 # hikisu kazu test
 echo requiared 2 numbers > $tmp-ans
-./17745145.sh 1 2 30 40 > $tmp-err && ERROR_EXIT "we can't script"
+./kadai2.sh 1 2 30 40 > $tmp-err && ERROR_EXIT "we can't script"
 diff $tmp-ans $tmp-err || ERROR_EXIT "17745145 hikisu correct?"
 
 echo "test4 OK"
